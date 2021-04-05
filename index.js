9681fbdd147d5d0e5def6d4724e5268fed36e86a
Francisco Castillo <Francisco21castillo@aol.com>
2021-04-05 10:39:21 -0400

    Completed

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Code your solution in this file!
+let companyName = 'Scuber';
+
+function nameTime(){
+    const companyName = 'live';
+}
+
+let mostProfitableNeighborhood = 'Chelsea';
+let companyCeo = 'Susan Smith';
